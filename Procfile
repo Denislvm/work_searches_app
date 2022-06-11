92030289bab29b18c594be91cccd37362a998d34
Denis Denis <slyusarenkodenisss@gmail.com>
2022-06-11 13:49:13 +0300

    add settings to deploy heroku

---
+++ Procfile
@@ -0,0 +1,1 @@
+web gunicorn thesisproject.wsgi:application --log-file -
--- thesisproject/thesisproject/settings.py
+++ thesisproject/thesisproject/settings.py
@@ -11,7 +11,8 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 import os
 from pathlib import Path
-
+import django_heroku
+import dj_database_url
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -23,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-%=2jm97fsf4^_%qw)g^m)u=p56=v#qe10#2el#-dp477k67tuj'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -120,6 +121,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = []
+django_heroku.settings(locals())
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
